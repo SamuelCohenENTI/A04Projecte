12df8c8bcd82c65ee98d4abeafcd753d0552210b
Julsi06 <julia.sanchez@enti.cat>
2025-02-17 13:54:27 +0100

    function

---
+++ function/exemple.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void Print(string parametro) {
+
+	cout << parametro;
+}
+
+void main() {
+	std::string parametro = "Hola";
+
+	Print(parametro);
+}
